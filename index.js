db72e38523472be5c4c451221ab042f52f1fdf8a
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-05-18 22:07:59 +0100

    make api requests with node-fetch

--- index.js
+++ index.js
@@ -14,3 +14,33 @@ app.listen(port, () => {
 });
 
 const api_key = process.env.API_KEY;
+
+app.get("/username", async (request, response) => {
+    const fetchApi = await fetch(
+        "https://random-user-by-api-ninjas.p.rapidapi.com/v1/randomuser", 
+        {
+            method: "GET",
+            headers: {
+                "x-rapidapi-key": api_key,
+                "x-rapidapi-host": "random-user-by-api-ninjas.p.rapidapi.com",
+            },
+        }
+    );
+    const userNameResponse = await fetchApi.json();
+    response.json(userNameResponse);
+});
+
+app.get("/userimage", async (request, response) => {
+    const fetchApi = await fetch(
+        "https://bing-image-search1.p.rapidapi.com/images/search?q=user&count=20", 
+        {
+            method: "GET",
+            headers: {
+                "x-rapidapi-key": api_key,
+                "x-rapidapi-host": "bing-image-search1.p.rapidapi.com",
+            },
+        }
+    );
+    const userImageResponse = await fetchApi.json();
+    response.json(userImageResponse);
+})
